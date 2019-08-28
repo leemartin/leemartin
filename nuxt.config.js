e3dafea7c930dedb81db398bdf7f187f7bc6b625
Lee Martin <hi@leemartin.com>
2019-08-28 14:08:48 -0500

    Added basic head elements

---
+++ nuxt.config.js
@@ -0,0 +1,19 @@
+export default{
+  head: {
+    title: 'Lee Martin / Developer',
+    link: [
+      {
+        rel: 'stylesheet',
+        href: 'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css'
+      }
+    ],
+    meta: [
+      {
+        charset: 'utf-8'
+      }, {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      }
+    ]
+  }
+}
