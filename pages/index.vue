d9e86428819e0d9299b6d26859a90406c59a8837
Lee Martin <hi@leemartin.com>
2019-08-28 14:16:51 -0500

    Added header and main sections

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,41 @@
 <template>
-  <h1>Lee Martin</h1>
+  <main>
+    <!-- Header -->
+    <!-- ========== -->
+    <header>
+      <h1>Lee Martin</h1>
+
+      <nav>
+        <a href="#">About</a>
+        <a href="#">Work</a>
+        <a href="#">Contact</a>
+        <a href="#">Newsletter</a>
+      </nav>
+    </header>
+
+    <!-- Intro -->
+    <!-- ========== -->
+    <section id="intro">
+      <h2>Lee Martin develops websites for rock n roll bands and gets paid in sex and drugs.</h2>
+      <a href="#">Read Newsletter</a>
+    </section>
+
+    <!-- About -->
+    <!-- ========== -->
+    <section id="about">
+      <h3>Hello</h3>
+    </section>
+
+    <!-- Work -->
+    <!-- ========== -->
+    <section id="work">
+      <h3>Selected Work</h3>
+    </section>
+
+    <!-- Contact -->
+    <!-- ========== -->
+    <section id="contact">
+      <h3>Get in touch</h3>
+    </section>
+  </main>
 </template>
