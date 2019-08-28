1422005df751f5d6d357d4e88e68754a77a6a10d
Lee Martin <hi@leemartin.com>
2019-08-28 15:25:36 -0500

    Created base CSS layout and sizing

--- pages/index.vue
+++ pages/index.vue
@@ -3,47 +3,63 @@
     <!-- Header -->
     <!-- ========== -->
     <header>
-      <h1>Lee Martin</h1>
+      <article>
+        <h1>Lee Martin</h1>
 
-      <nav>
-        <a href="#">About</a>
-        <a href="#">Work</a>
-        <a href="#">Contact</a>
-        <a href="https://confirmsubscription.com/h/j/7BE1CD1F96F6B42F" target="_blank">Newsletter</a>
-      </nav>
+        <nav>
+          <a href="#">About</a>
+          <a href="#">Work</a>
+          <a href="#">Contact</a>
+          <a href="https://confirmsubscription.com/h/j/7BE1CD1F96F6B42F" target="_blank">Newsletter</a>
+        </nav>
+      </article>
     </header>
 
     <!-- Intro -->
     <!-- ========== -->
     <section id="intro">
-      <h2>Lee Martin develops websites for rock n roll bands and gets paid in sex and drugs.</h2>
-      <a href="https://leemartin.createsend.com/campaigns/reports/viewCampaign.aspx?d=j&c=9B0B724E2C919577&ID=5B4E63BEA43BA3E22540EF23F30FEDED&temp=False&tx=0" target="_blank">Read latest newsletter</a>
+      <article>
+        <h2>Lee Martin develops websites for rock n roll bands and gets paid in sex and drugs.</h2>
+        <a href="https://leemartin.createsend.com/campaigns/reports/viewCampaign.aspx?d=j&c=9B0B724E2C919577&ID=5B4E63BEA43BA3E22540EF23F30FEDED&temp=False&tx=0" target="_blank">Read latest newsletter</a>
+      </article>
     </section>
 
     <!-- About -->
     <!-- ========== -->
     <section id="about">
-      <h3>Hello</h3>
-      <h4>My name is Lee Martin, a designer & developer, with more than 16 years of experience in the music business.</h4>
+      <article>
+        <h3>Hello</h3>
+        <h4>My name is Lee Martin, a designer & developer, with more than 16 years of experience in the music business.</h4>
+        <p>I started my career by running a band's <a href="http://leemart.in/origin">fan site</a> during college and used my connections there (and some blind luck) to begin freelancing in the music business. I took my first real job at <a href="http://www.sammusicbiz.com">Silva Artist Management</a> where I headed up their one-man new media department, crafting campaigns for Foo Fighters, Beck, Jimmy Eat World, Them Crooked Vultures, and many others.</p>
+        <p>After two years at SAM, I became the first US employee of SoundCloud in the position of Experimental Developer, doing business development by building VIP artist campaigns and scalable apps that utilized SoundCloud's platform. Three years later, I've now exited SoundCloud and reunited with my old band: Freelancing.</p>
+        <p>I enjoy creating accessible and focused projects that utilize new web technologies to engage fans in interactive ways and allow them to participate in the storytelling process. I feel this makes fans more inclined to share their participation and in turn expands the viral reach of the campaign's goals.</p>
+        <p>Have a project in mind? Please, <a href="#">get in touch</a>.</p>
+      </article>
     </section>
 
     <!-- Work -->
     <!-- ========== -->
     <section id="work">
-      <h3>Selected Work</h3>
+      <article>
+        <h3>Selected Work</h3>
+        <p>For more case studies, please visit <a href="https://medium.com/@leemartin">the blog</a>.</p>
+      </article>
     </section>
 
     <!-- Contact -->
     <!-- ========== -->
     <section id="contact">
-      <h3>Get in touch</h3>
-      <p>+1 985 209 1111<br><a href="mailto:hi@leemartin.com" target="_blank">hi@leemartin.com</a></p>
+      <article>
+        <h3>Get in touch</h3>
+        <p>+1 985 209 1111<br><a href="mailto:hi@leemartin.com" target="_blank">hi@leemartin.com</a></p>
 
-      <nav>
-        <a href="https://www.twitter.com/leemartin">Twitter</a>
-        <a href="https://www.facebook.com/leemartin">Facebook</a>
-        <a href="https://www.linkedin.com/in/leepaulmartin">LinkedIn</a>
-      </nav>
+        <nav>
+          <a href="https://www.twitter.com/leemartin">Twitter</a>
+          <a href="https://www.facebook.com/leemartin">Facebook</a>
+          <a href="https://www.linkedin.com/in/leepaulmartin">LinkedIn</a>
+          <a href="https://medium.com/@leemartin">Medium</a>
+        </nav>
+      </article>
     </section>
   </main>
 </template>
